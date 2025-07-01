106eeda2a706a3924416c7e11ee04e34fb0df607
aman-3827 <112684265+aman-3827@users.noreply.github.com>
2025-07-01 23:08:43 +0530

    Update API key
    weatherapi

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ document.querySelector(".weather-info").append(weatherCondition, humidity, windS
 
 async function getData(city) {
     try {
-        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=3f12428bfa494f85b7015659240704=${city}&aqi=yes`);
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=71e3873581f04e7fa15173013250107&q=${city}&aqi=yes`);
+
         
         if (!response.ok) {
             throw new Error("City not found");
